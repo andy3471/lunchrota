48e53a7a0a5b9917e88e72a132e370148f274af1
Andrew Hargrave <Andrew.Hargrave@oneadvanced.com>
2019-11-07 14:10:04 +0000

    Migrations

---
+++ app/Http/Controllers/LunchSlotController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LunchSlot;
+use Illuminate\Http\Request;
+
+class LunchSlotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LunchSlot  $lunchSlot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LunchSlot $lunchSlot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LunchSlot  $lunchSlot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LunchSlot $lunchSlot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LunchSlot  $lunchSlot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LunchSlot $lunchSlot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LunchSlot  $lunchSlot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LunchSlot $lunchSlot)
+    {
+        //
+    }
+}
