a86eb1ec48f7d31d13ebe353ddc44840f436b070
andy3471 <andy@insideindie.net>
2020-02-14 18:25:57 +0000

    Fixed #63

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(App\User::class)->make([
-            'name' => 'admin',
             'email' => 'admin@admin.com',
             'admin' => '1',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
