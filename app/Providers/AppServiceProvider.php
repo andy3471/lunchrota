209cad3e7612b5ed797e55765f967c590e0b685a
Andrew Hargrave <me@andyh.app>
2024-11-06 16:49:27 +0000

    prevent lazy load

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-    }
-
-    public function boot()
-    {
+        Model::preventLazyLoading(app()->isLocal());
     }
 }
