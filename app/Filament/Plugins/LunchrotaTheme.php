9426f5b5b9279b04e6b456502f2dc774b59dff3e
Andrew Hargrave <me@andyh.app>
2025-05-19 15:56:51 +0100

    add octane

--- app/Filament/Plugins/LunchrotaTheme.php
+++ app/Filament/Plugins/LunchrotaTheme.php
@@ -10,9 +10,9 @@ class LunchrotaTheme extends MinimalTheme
     {
         return [
             'primary' => '#fec107',
-            'danger'  => '#d97706',
-            'gray'    => '#4b5563',
-            'info'    => '#3b82f6',
+            'danger' => '#d97706',
+            'gray' => '#4b5563',
+            'info' => '#3b82f6',
             'success' => '#10b981',
             'warning' => '#f97316',
         ];
