a4d2faa5a7b4c044b40f3308346efb7923e7e0a9
Shift <shift@laravelshift.com>
2025-05-19 15:20:14 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\RolesGenerate;
+use App\Console\Commands\RolesClear;
+use App\Console\Commands\DemoModeRefresh;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command(DemoModeRefresh::class)
+    ->daily();
+
+Schedule::command(RolesGenerate::class)
+    ->daily();
+
+Schedule::command(RolesClear::class)
+    ->daily();
