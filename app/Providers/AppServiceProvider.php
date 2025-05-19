159c75b9984253edbadbfe6d5f9261f5a2e3483f
Shift <shift@laravelshift.com>
2025-05-19 15:20:12 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,41 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     public function register()
     {
         Model::preventLazyLoading(app()->isLocal());
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+
+        $this->bootAuth();
+    }
+
+    public function bootAuth()
+    {
+
+        Gate::define('admin', function ($user) {
+            return $user->admin;
+        });
     }
 }
