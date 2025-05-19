e5137d1d8ec08fb0a03c9aa3d3635581ef0e0fab
Shift <shift@laravelshift.com>
2025-05-19 15:20:12 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -23,25 +23,6 @@ return [
 
     'footer_text' => env('APP_FOOTER_TEXT', ''),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
